8a5d3ac5385b8af5fd9cdb2f383466f1260382ec
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-06 11:28:58 -0700

    creating table commands

---
+++ creating_tables.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE pet_shop;
+
+USE pet_shop; 
+
+SELECT DATABASE();
+
+
+CREATE TABLE cats (
+    name VARCHAR(100),
+    age INT
+);
+
+CREATE TABLE dogs (
+    name VARCHAR(100),
+    breed VARCHAR(50),
+    age INTEGER
+);
+
+--utility commands
+SHOW TABLES;
+
+SHOW COLUMNS FROM <table_name>;
+
+DESC <table_name> (or DESCRIBE)
