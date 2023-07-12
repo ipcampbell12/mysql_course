bee3c8b2cfd726171bb5a0097ce50fb442458871
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-12 09:57:52 -0700

    added data

--- shirts_crud.sql
+++ shirts_crud.sql
@@ -29,3 +29,20 @@ MODIFY color VARCHAR(50) NOT NULL;
 
 ALTER TABLE shirts
 MODIFY shirt_size CHAR(10) NOT NULL;
+
+--change a data type
+ALTER TABLE shirts
+MODIFY last_worn INTEGER;
+
+--add some data and verify it was added
+INSERT INTO shirts (article,color,shirt_size,last_worn)
+VALUES  ('t-shirt', 'white', 'S', 10),
+        ('t-shirt', 'green', 'S', 200),
+        ('polo shirt', 'black', 'M', 10),
+        ('tank top', 'blue', 'S', 50),
+        ('t-shirt', 'pink', 'S', 0),
+        ('polo shirt', 'red', 'M', 5),
+        ('tank top', 'white', 'S', 200),
+        ('tank top', 'blue', 'M', 15);
+
+SELECT * FROM shirts;
