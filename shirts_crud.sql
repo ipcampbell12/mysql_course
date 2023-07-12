c1d6c1751ef906ee31ed0cf24f916668aaf30793
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-12 10:06:10 -0700

    even more crud operations

--- shirts_crud.sql
+++ shirts_crud.sql
@@ -75,3 +75,13 @@ WHERE last_worn=15;
 
 UPDATE shirts SET last_worn = 0
 WHERE last_worn = 15;
+
+
+--change white to off white
+SELECT * FROM shirts
+WHERE color='white';
+
+UPDATE shirts SET shirt_size = 'XS', color='off white'
+WHERE color='white';
+
+SELECT * FROM shirts;
