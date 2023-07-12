8833fc1bfb99a9a050d9144d464ceada41236a79
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-12 08:54:43 -0700

    where clause

---
+++ where.sql
@@ -0,0 +1,31 @@
+
+SELECT * FROM cats
+WHERE age >= 9;
+
+SELECT name,age FROM cats
+WHERE age >= 9;
+
+
+--dont' have to use column in select even if it is in where
+SELECT name FROM cats
+WHERE age >= 9;
+
+
+--WHERE is executed first 
+
+--cant be case insensitive ('Egg' is the actual name)
+SELECT * FROM cats
+WHERE name = 'egg';
+
+
+SELECT id FROM cats;
+
+SELECT name, breed FROM cats;
+
+
+--actual breed name is 'Tabby' it's case inesensitive
+SELECT name, age FROM cats
+WHERE breed = 'tabby';
+
+SELECT id, age FROM cats
+WHERE id=age;
