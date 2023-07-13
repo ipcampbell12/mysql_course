2d9a43e4b1b02cffe7989470ca7fcf59c7581a0a
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-13 10:45:28 -0700

    upper lower

---
+++ upper_lower.sql
@@ -0,0 +1,15 @@
+SELECT
+    UPPER('hello');
+
+SELECT
+    LOWER('HELLO');
+
+SELECT
+    UPPER(title)
+FROM
+    books;
+
+SELECT
+    CONCAT('I LOVE ', UPPER(title), ' !!!') AS love_title
+FROM
+    books;
