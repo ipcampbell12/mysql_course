c48030daef0d4b934f35a4164334f6aa8b0feae4
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-13 10:26:09 -0700

    replace

---
+++ replace.sql
@@ -0,0 +1,16 @@
+-- replace function
+-- 3 different string arguments
+-- 1. string operating on
+-- 2. part we want to replace
+-- 3. what we want to replace it with
+-- this function is case sensitive
+SELECT
+    REPLACE('Hello World', 'Hell', '%$#@');
+
+SELECT
+    REPLACE('cheese bread coffee milk', ' ', ' and ');
+
+SELECT
+    REPLACE(title, ' ', '-')
+FROM
+    books;
