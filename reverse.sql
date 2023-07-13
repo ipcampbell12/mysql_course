3a4dd493acdd3811bf77b248010054d240d4254b
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-13 10:30:09 -0700

    reverse

---
+++ reverse.sql
@@ -0,0 +1,13 @@
+-- SELECT
+--     REVERSE('chicken nuggets');
+-- pretty straightfoward
+SELECT
+    REVERSE(author_fname)
+FROM
+    books;
+
+-- turn every person's name into a palindrome
+SELECT
+    CONCAT(author_fname, REVERSE(author_fname))
+FROM
+    books;
