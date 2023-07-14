4db0cea51f0cc84441ac726da20fb2e7c66108d7
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-14 12:29:24 -0700

    more order by

--- refininq_selection/order_by.sql
+++ refininq_selection/order_by.sql
@@ -7,4 +7,45 @@ SELECT
 FROM
     books
 ORDER BY
-    author_lname;
+    author_lname DESC;
+
+SELECT
+    author_fname,
+    author_lname,
+    title,
+    pages
+FROM
+    books
+ORDER BY
+    released_year DESC;
+
+-- order by number = order by that number column that you have selected
+SELECT
+    title,
+    author_fname,
+    pages
+FROM
+    books
+ORDER BY
+    1;
+
+-- can also order by multiple columns
+-- sort by 1 column, and then by the next one (not together)
+-- and one can be ascending and the ohter descending
+SELECT
+    author_lname,
+    released_year,
+    title
+FROM
+    books
+ORDER BY
+    author_lname,
+    title;
+
+-- can use column aliases to order by; can reference column aliases in ORDER BY clause
+SELECT
+    CONCAT(author_fname, ' ', author_lname) AS author
+FROM
+    books
+ORDER BY
+    author;
