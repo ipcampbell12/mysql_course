b5380d9c9f7896de4f42ba11de63bd8fce832596
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-15 16:52:28 -0700

    count

---
+++ aggregate_functions/count.sql
@@ -0,0 +1,32 @@
+-- count(*) means how many rows we get back from this query
+SELECT
+    COUNT(*) AS total
+FROM
+    books;
+
+-- boils whole result down into a single value
+-- can pass distinct into parens to know how many author fnames
+SELECT
+    COUNT(DISTINCT author_fname)
+FROM
+    books;
+
+-- how many unique released years
+SELECT
+    COUNT(DISTINCT released_year)
+FROM
+    books;
+
+-- count distinct last names
+SELECT
+    COUNT(DISTINCT author_lname)
+FROM
+    books;
+
+-- find out the number of titles that contain the word 'the' 
+SELECT
+    COUNT(*) AS contains_the
+FROM
+    books
+WHERE
+    title LIKE '%the%';
