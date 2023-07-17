504caaed0ec1e9de196d726cfdd85869ddf467a6
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-17 15:29:51 -0700

    current date/time

---
+++ data_types/cur_date.sql
@@ -0,0 +1,19 @@
+-- current_date, current_time, current_timestamp
+
+-- cur_date, cur_time, now() (shortened versions of the above)
+
+
+-- current time
+SELECT CURTIME();
+
+-- current date
+SELECT CURDATE();
+
+-- current datetime
+SELECT NOW();
+
+-- someone was just born
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES ('Hazel',CURDATE(),CURTIME(),NOW());
+
+SELECT * FROM people;
