2089185892ce257f1464f281ec34677fdc4cccfe
ipcampbell12 <ipcampbell12@gmail.com>
2023-07-18 14:16:57 -0700

    format date

---
+++ data_types/format_date.sql
@@ -0,0 +1,23 @@
+-- we want this format: ' April 11 1985'
+
+
+-- cant use concat
+SELECT CONCAT(MONTHNAME(birthdate),' ',DAYOFMONTH(birthdate),' ',YEAR(birthdate)) AS birthdate
+FROM people;
+
+-- better to use DATE_FORMAT()
+-- two arguments: date that you are formatting, and the format string
+
+SELECT birthdate, DATE_FORMAT(birthdate,'%a %b %D %Y') FROM people;
+
+-- also similar function for TIME_FORMAT
+
+SELECT birthdt, DATE_FORMAT(birthdt, '%H:%i') FROM people;
+
+-- for 12 hour time with AM and PM
+
+SELECT birthdt, DATE_FORMAT(birthdt, '%r') FROM people;
+
+-- can also add text
+
+SELECT birthdt, DATE_FORMAT(birthdt, 'Born at %r ') FROM people;
